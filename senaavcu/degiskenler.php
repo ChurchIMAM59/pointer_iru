1b6e56214ce13035c00f49feeb44584c5b5a7e2c
ChurchIMAM59 <63426586+ChurchIMAM59@users.noreply.github.com>
2020-04-19 21:14:33 +0300

    BatuhanH
    proje teslim

---
+++ senaavcu/degiskenler.php
@@ -0,0 +1,45 @@
+<?php
+
+//******************************
+// ! ! DOKUNMA ! !
+$user="root";
+$pass ="";
+$data = "rumeliedu";
+$sunucu="localhost";
+$tablo_adi ="senaavcu";
+$page_id="1";
+$page_title="Merhaba Dünya";
+$page_img="";
+$page_kategori="";
+$page_text="";
+$page_info="";
+
+// ***************************
+// ***************************
+// ***************************
+// ***************************
+// ***************************
+
+$user_bio ="
+
+
+	02 Haziran 2000 yılında İstanbul'un Çatalca ilçesinde doğdum ve hala Çatalca'da yaşıyorum.
+Şu anda 19 yaşındayım. İstanbul Rumeli Üniversitesinde 1. sınıf öğrencisiyim. Bilgisayar Programcılığı bölümünde okuyorum.
+Gün içerisinde bilgisayarla ilgilenirim.Kendimi daha fazla geliştirmek için elimden geleni yaparım.
+Resim çizmeyi, fotoğraf çekmeyi, dans etmeyi, bilgisayasar ve telefon oyunları oynamayı, arkadaşlarımla bir arada olmayı
+ve dizi izlemeyi çok severim.
+
+
+  ";
+$user_adsoyad="Sena Avcu";
+$user_mail ="senaavcuu2@gmail.com";
+$user_gsm ="";
+$user_sehir="Çatalca / İSTANBUL";
+$user_adres = "";
+$user_facebook ="";
+$user_twitter ="https://twitter.com/senaavcuu1";
+$user_insta = "https://www.instagram.com/senaaavcuu/";
+$user_git = "https://github.com/senaavcu/";
+
+
+ ?>
